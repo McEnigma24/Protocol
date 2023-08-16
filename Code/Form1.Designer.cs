c08f2f87871eea89947af5ee29281d43fe21b485
McEnigma24 <116126603+McEnigma24@users.noreply.github.com>
2023-08-16 23:08:41 +0200

    Add files via upload

---
+++ Code/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace Shutdown_Protocol
+{
+    partial class Shutdown_Protocol
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_user_text = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_user_text
+            // 
+            this.textBox_user_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.textBox_user_text.Location = new System.Drawing.Point(30, 50);
+            this.textBox_user_text.Multiline = true;
+            this.textBox_user_text.Name = "textBox_user_text";
+            this.textBox_user_text.Size = new System.Drawing.Size(237, 193);
+            this.textBox_user_text.TabIndex = 0;
+            this.textBox_user_text.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(96, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Protocol";
+            // 
+            // Shutdown_Protocol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 264);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_user_text);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Shutdown_Protocol";
+            this.Text = "Shutdown Protocol";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_user_text;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Code/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace Shutdown_Protocol
+{
+    public partial class Shutdown_Protocol : Form
+    {
+        private XmlSerializer serializer;
+        private string serialize_dir;
+
+        private string user_text_dir;
+        private List<string> list_user_protocol;
+        private bool last_chance = true;
+
+        public Shutdown_Protocol()
+        {
+            InitializeComponent();
+
+            this.ActiveControl = textBox_user_text;
+
+            serialize_dir = @"Application Files/properties.txt";
+            user_text_dir = @"Application Files/user text.txt";
+
+            list_user_protocol = new List<string>();
+
+
+
+            // setting user protocol
+            initProtocolList();
+            updateTextBox();
+
+            // applying serialized preferences
+            {
+
+            }
+        }
+
+
+        private void initProtocolList()
+        {
+            string[] file = File.ReadAllLines(user_text_dir);
+
+            int i = 1;
+            string tmp_line;
+            string dummy_line;
+            foreach (string line in file)
+            {
+                dummy_line = line;
+                tmp_line = "";
+
+                if (dummy_line != "")
+                {
+                    if (!dummy_line.Contains('&')) tmp_line += i.ToString() + ". ";
+                    else dummy_line = dummy_line.Replace("&", " ");
+                    i++;
+                }                
+                tmp_line += dummy_line;
+
+                //if (tmp_line.Length > 0) list_user_protocol.Add(tmp_line.Remove(tmp_line.IndexOf('&'), 1));
+                //else
+                list_user_protocol.Add(tmp_line);
+            }
+        }
+        
+        private void updateTextBox()
+        {
+            textBox_user_text.Text = "";
+            int full_size = list_user_protocol.Count();            
+
+            foreach (string line in list_user_protocol)
+            {
+                textBox_user_text.AppendText(line);
+
+                if(full_size != 0) textBox_user_text.AppendText(Environment.NewLine);
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {                
+                if (!last_chance)                    
+                {
+                    textBox_user_text.Text = "SHUTDOWN";
+                    //Process.Start("shutdown", "/s /t 0");
+                    return;
+                }                
+
+                /*if (list_user_protocol.Count() == 0)
+                {
+                    textBox_user_text.Text = "no longer shutdown";
+                    return;
+                }*/
+
+                removingEmptyElements();
+                {
+                    list_user_protocol.RemoveAt(0);
+                }
+                removingEmptyElements();
+
+                updateTextBox();
+
+                if (list_user_protocol.Count() == 0)
+                {
+                    if (last_chance)
+                    {
+                        textBox_user_text.Text = "SHUTDOWN AFTER THIS";
+                        last_chance = false;
+                    }
+                }
+            }
+        }
+
+        private void removingEmptyElements()
+        {
+            if (list_user_protocol.Count() != 0)
+            {
+                while (list_user_protocol.ElementAt(0) == "")
+                    list_user_protocol.RemoveAt(0);
+            }
+        }
+    }
+}
